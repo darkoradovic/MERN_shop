61a5862c38ecd310fe301f1bc5b5964cbbf8d4da
Darko Radovic <darmio5@gmail.com>
2020-10-14 17:02:25 +0200

    Created Header and Footer components, created Homescreen screen and Rating componet

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = props => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${props.product._id}`}>
+        <Card.Img src={props.product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${props.product._id}`}>
+          <Card.Title as="div">
+            <strong>{props.product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating
+            value={props.product.rating}
+            text={`${props.product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">$ {props.product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
